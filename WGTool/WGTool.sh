5110542591699708a94362eb8447ba707ef0e997
amber <amberle361@gamil.com>
2021-05-12 23:27:20 +0800

    [Mod]:修改shell脚本逻辑

--- WGTool/WGTool.sh
+++ WGTool/WGTool.sh
@@ -1,27 +1,46 @@
 
 #!/bin/bash
-# get all filename in specified path
-
-path=$PWD
-echo "当前路径->$path"
-
-time=$(date "+%Y%m%d")
-echo "当前时间->$time"
-
-filename=$(ls $path|grep $time)
-echo "获取到的文件名->$filename"
-
-if [ -z "$filename" ]; then
-    echo "未获取到当天的配置，\n请打开联通手机营业厅重新连接试试，请确认使用的是自动输出日志的版本"
-else
-    conf=$(cat $filename|grep ^{)
-    echo "获取到的沃游戏配置->$conf"
-fi
-
-exit 0
 
 
+# 检测当前时钟，凌晨0点到3点不更新，给服务器节省资源
+checkHours(){
+    hour=$(date "+%k")
+    if [ $hour -le 3 ]; then
+        echo "凌晨3点之后再执行脚本吧"
+        sleep 0.5
+        exit;
+    else
+        echo "时间校验通过"
+        sleep 0.5
+    fi
+}
 
+# 找出当天配置文件
+findConf(){
+    time=$(date "+%Y%m%d")
+    echo "当天日期：$time"
+    sleep 0.5
+    path=$PWD
+    echo "当前路径：$path"
+    sleep 0.5
+    filename=$(ls $path|grep $time)
+    if [[ -z "$filename" ]]; then
+        echo "未获取到当天的配置，\n请打开联通手机营业厅重新连接试试，请确认使用的是自动输出日志的版本"
+        exit;
+    else
+        echo "获取到的文件名：$filename"
+        conf=$(cat $filename|grep ^{)
+        echo "获取到的沃游戏配置->$conf"
+    fi
+}
+
+main (){
+    checkHours
+    echo "准备查找配置了"
+    findConf
+}
+
+main
 
 
 # path=$1
